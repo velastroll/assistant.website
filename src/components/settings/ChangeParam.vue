341a877feca381ef2040aa0f4c7b4680de0ece13
alvvela <alvaro.velasco.gil@alumnos.uva.es>
2020-03-04 20:45:48 +0100

    add : wifi credentials

--- src/components/settings/ChangeParam.vue
+++ src/components/settings/ChangeParam.vue
@@ -39,6 +39,27 @@
         updates it when receive
         <strong>CONF</strong> event.
       </a>
+      <hr />
+      <!-- configure wifi -->
+      <div class="content-card">
+        <b-row class="inner-row">
+          <a class="row-text font-weight-bold">WIFI SSID</a>
+          <b-input
+            v-model="ssid"
+            :state="checkssid()"
+            style="width: 10rem; border-radius: 0.5rem; height: 1.5rem; text-align: right;"
+          />
+          <a class="row-text font-weight-bold">PASSWORD</a>
+          <b-input
+            :state="checkssid_ps()"
+            v-model="ssid_ps"
+            type="password"
+            style="width: 10rem; border-radius: 0.5rem; height: 1.5rem; text-align: right;"
+          />
+          <b-button class="btn-wifi" :disabled="!checkwifi()" @click="sentWifi()">ENVIAR</b-button>
+        </b-row>
+      </div>
+      <!-- end configure wifi -->
     </div>
     <!-- time to next ping -->
     <div class="content-card">
@@ -63,13 +84,14 @@
     <div class="center">
       <b-button v-if="edit" class="save-btn" @click="createsConfig()">SAVE</b-button>
       <div v-else style="width: 100%">
-      <b-button @click="edit = !edit" class="edit-btn">EDIT</b-button>
-      <b-button @click="sentConfEvent()" class="conf-btn">
-        sent <strong> conf </strong> event for 
-        <a v-if="receiver=='GLOBAL'"> all devices</a>
-        <a v-else-if="receiver > 1"> this location devices</a>
-        <a v-else> this device </a>
-      </b-button>
+        <b-button @click="edit = !edit" class="edit-btn">EDIT</b-button>
+        <b-button @click="sentConfEvent()" class="conf-btn">
+          sent
+          <strong>conf</strong> event for
+          <a v-if="receiver=='GLOBAL'">all devices</a>
+          <a v-else-if="receiver > 1">this location devices</a>
+          <a v-else>this device</a>
+        </b-button>
       </div>
     </div>
   </div>
@@ -87,6 +109,8 @@ export default {
   },
   data() {
     return {
+      ssid : '',
+      ssid_ps : '',
       edit: false,
       new_sleep_sec: 0,
       new_config: {
@@ -98,7 +122,29 @@ export default {
     };
   },
   methods: {
-    makeToast(variant, title, content){
+    checkssid(){ return this.ssid.length > 6},
+    checkssid_ps(){ return this.ssid_ps.length > 7},
+    checkwifi(){ return this.checkssid() && this.checkssid_ps() },
+    sentWifi() {
+      this.$store
+        .dispatch("tasks/new", {
+          event: "WIFI",
+          device: this.receiver,
+          content : this.ssid + ";" + this.ssid_ps
+        })
+        .then(r => {
+          if (r.status == 200) {
+            this.makeToast(
+              "success",
+              "Added",
+              `New task assigned to [${this.receiver}]`
+            );
+          } else {
+            this.makeToast("danged", `oups ${r.status}`, `${r.data}`);
+          }
+        });
+    },
+    makeToast(variant, title, content) {
       this.$bvToast.toast(content, {
         title: title,
         variant: variant,
@@ -113,11 +159,7 @@ export default {
           .dispatch("confs/sendConfiguration", this.new_config)
           .then(res => {
             if (res.status == 200) {
-              this.makeToast(
-                "success",
-                "Success",
-                "New configuration added."
-              );
+              this.makeToast("success", "Success", "New configuration added.");
               this.edit = false;
               this.$parent.$parent.refresh();
             } else {
@@ -132,32 +174,42 @@ export default {
         );
       }
     },
-    sentConfEvent(){
-      this.$store.dispatch("tasks/new", {
-        event: 'CONF',
-        device: this.receiver
-      }).then(r => {
-        if(r.status == 200){ 
-          this.makeToast(
-            "success",
+    sentConfEvent() {
+      this.$store
+        .dispatch("tasks/new", {
+          event: "CONF",
+          device: this.receiver
+        })
+        .then(r => {
+          if (r.status == 200) {
+            this.makeToast(
+              "success",
               "Added",
               `New task assigned to [${this.receiver}]`
             );
-        } else {
-                    this.makeToast(
-            "danged",
-              `oups ${r.status}`,
-              `${r.data}`
-            );
-        }
-
-          });
+          } else {
+            this.makeToast("danged", `oups ${r.status}`, `${r.data}`);
+          }
+        });
     }
   }
 };
 </script>
 
 <style scoped>
+.btn-wifi {
+  width: 5rem;
+  border-radius: 0.5rem;
+  height: 1.5rem;
+  text-align: center;
+  margin-left: 2rem;
+  padding: 0rem 0rem;
+  background: #2c3e50;
+}
+.btn-wifi:hover{
+  background: #4673a0
+}
+
 .save-btn {
   border-radius: 1rem;
   border: solid darkgreen 1px;
@@ -189,7 +241,7 @@ export default {
   border: solid orange 1px;
 }
 
-.conf-btn{
+.conf-btn {
   text-transform: uppercase;
   margin-top: 0.5rem;
   border-radius: 1rem;
@@ -206,7 +258,6 @@ export default {
   color: darkgreen;
   border: solid darkgreen 1px;
 }
-
 
 div.content-card {
   margin: 1rem 0 0 0;
